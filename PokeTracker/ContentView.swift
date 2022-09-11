4e3866f6efc1cc22c1344b477c58f8a55e01cb37
Joakim Stien <joakim.stien@icloud.com>
2022-09-11 13:42:21 +0200

    Initial Commit

---
+++ PokeTracker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PokeTracker
+//
+//  Created by Joakim Stien on 11/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PokeTracker/PokeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeTrackerApp.swift
+//  PokeTracker
+//
+//  Created by Joakim Stien on 11/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
